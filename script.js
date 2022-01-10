5b6104dab5a85a3c18e9839d8760054293545540
janeToomey <j.toomey1027@gmail.com>
2022-01-09 18:07:41 -0800

    js variables added

---
+++ script.js
@@ -0,0 +1,20 @@
+var generatebtn = document.querySelector("#generate");
+
+//variables
+
+//var length = Number(prompt("Enter a password length between 8 and 128"));
+var lowerChar = {
+    lowerChar: "abcdefghijklmnopqrstuvwxyz"};
+var upperChar = {
+    uppercase:"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+var numberChar = {
+      numeric:"0123456789"};
+var specialChar = {
+      special:"!@#$%^&*?/-~:;"};
+
+var length="";
+var userLower;
+var userUpper;
+var userNumbers;
+var userSpChar;
+
